4fd76ebbc43623e4437c79c22bcccaf58f57ff68
Thaysa Mara  Monteiro dos Santos <thaysamarasantos@gmail.com>
2017-10-09 14:45:36 -0300

    adding contact section and updating layout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any;
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  goToAbout(){
+    jQuery('html, body').stop().animate({
+      scrollTop: jQuery("#about").offset().top //set offset to scroll
+    }, 1000);
   }
 
 }
