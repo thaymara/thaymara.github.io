8df5c576a5133edb7ee94506f961eb19cdb5e698
Thaysa Santos <thaysamarasantos@gmail.com>
2017-12-08 11:48:48 -0200

    Fix site to work on IE

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -2,5 +2,5 @@ export class AppSettings {
     public static CREATEDYEAR = "2017";
     public static SITENAME = "thaymara.github.io";
     public static USERNAME = "thaymara"
-    public static GITURL = `https://api.github.com/users/${AppSettings.USERNAME}/repos?sort=updated`;
+    public static GITURL = `https://api.github.com/users/${AppSettings.USERNAME}/repos?sort=updated&type=owner`;
 }
